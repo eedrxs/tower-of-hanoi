08ac3f5333d3a7f89df5d028fd4fa3bd73eb7f47
theBardBoy <alabialade@gmail.com>
2021-10-05 17:30:06 +0100

    Tested out the iteration HTML structure

--- JavaScript/scripts/hanoi.js
+++ JavaScript/scripts/hanoi.js
@@ -1,22 +1,45 @@
-function setUpArrays(n) {
-  let source = new Array(n)
-    , auxiliary = new Array(n)
-    , destination = new Array(n);
-    
-    for (let item of source) item = {};
+'use strict';
+
+let noOfDisks;
+let iterationBuffer = [];
+let colors;
+let move = 0;
+
+
+function go(n) {
+  colors = new Array(n);
+
+  {
+    let hue = Math.trunc(300/n);
+    let mix = 0;
+
+    for (let i = 0; i < n; i++) {
+    colors[i] = `hsl(${mix += hue}, 100%, 50%)`;
+    }
+  }
+
+  towerOfHanoi(n, 'src', 'dest', 'aux');
 }
 
 
-function towerOfHanoi(n, from_rod,  to_rod,  aux_rod)
-{
-        if (n == 1)
-        {
-            document.write("Move disk 1 from rod " + from_rod +
-            " to rod " + to_rod+"<br/>");
-            return;
-        }
-        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-        document.write("Move disk " + n + " from rod " + from_rod +
-        " to rod " + to_rod+"<br/>");
-        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+function towerOfHanoi(n, from_rod, to_rod, aux_rod) {
+  if (n == 1) {
+      iterationBuffer[move] = new Iteration(++move, 1, from_rod, to_rod);
+      return;
     }
+
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    iterationBuffer[move] = new Iteration(++move, n, from_rod, to_rod);
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+
+
+class Iteration {
+  constructor(move, disk, from, to) {
+    this.move = move;
+    this.disk = disk;
+    this.from = from;
+    this.to = to;
+  }
+}
